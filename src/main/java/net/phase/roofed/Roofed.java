9b333df6e0092a494cdd28c3e69adf98930ece4c
PhaseZenith <vanoussebastian@outlook.cz>
2026-07-05 10:13:46 +0200

    Initial commit

---
+++ src/main/java/net/phase/roofed/Roofed.java
@@ -0,0 +1,23 @@
+package net.phase.roofed;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.util.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Roofed implements ModInitializer {
+	public static final String MOD_ID = "roofed";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.of(MOD_ID, path);
+	}
+}
---
+++ src/main/java/net/phase/roofed/RoofedClient.java
@@ -0,0 +1,10 @@
+package net.phase.roofed;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RoofedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
